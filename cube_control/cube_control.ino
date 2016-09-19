87424e62002da00ff093bde0d2953ac746756a11
Jasper O'Leary <jhaazpr@gmail.com>
2016-09-18 23:49:03 -0700

    trainwreck checkpoint

--- cube_control/cube_control.ino
+++ cube_control/cube_control.ino
@@ -33,12 +33,16 @@ double change_x_avg;
 double change_y_avg;
 double change_z_avg;
 
-const double JERK_THRESH_LOW = 6.0;
-const double JERK_THRESH_HIGH = 9.0;
-const int TIMEFRAME = 25;
-const int JOLT_WINDOW_MULTIPLIER = 4;
-int jolt_counter;
-void check_jerk(double *, double *, double *);
+const double JERK_THRESH_LOW = 3.0;
+const double JERK_THRESH_HIGH = 3.0;
+const int TIMEFRAME = 20;
+const int JOLT_WINDOW_MULTIPLIER = 1;
+unsigned long jolt_counter;
+const int JOLT_COOLDOWN_MAX = 20;
+int jolt_cooldown = 0;
+bool check_jerk(double *, double *, double *);
+
+sensors_event_t event;
 
 void setup(void) {
 
@@ -69,40 +73,22 @@ void loop() {
 //  Serial.print("  \tZ:  "); Serial.print(lis.z);
 
   /* Or....get a new sensor event, normalized */
-  sensors_event_t event;
   lis.getEvent(&event);
   curr_x = event.acceleration.x;
   curr_y = event.acceleration.y;
   curr_z = event.acceleration.z;
 
-   //Serial.print("X ori:  "); Serial.print(event.acceleration.x);
-  // Serial.print("  \tY ori:  "); Serial.print(event.acceleration.y);
-  // Serial.print("  \tZ ori:  "); Serial.print(event.acceleration.z);
+  // Serial.println("X accel:  "); Serial.print(event.acceleration.x);
+  // Serial.print("  \tY accel:  "); Serial.print(event.acceleration.y);
+  //Serial.print("  \tZ accel:  "); Serial.print(event.acceleration.z);
 
   /* Display the results (acceleration is measured in m/s^2) */
 
   // Serial.print(" \tchange in x: "); Serial.println(change_x);
 
-   if(change_y > JERK_THRESH_LOW){                      //change in accel y > 10
-     Serial.println("jolt y up");
-       Serial1.println("UP");
-   }
-   else if (change_y < -JERK_THRESH_LOW){             //change in accel y < -10
-        Serial.println("jolt y down");
-        // Serial1.println("jolt y down");
-         Serial1.println("DOWN");
-   } else if (change_x < -JERK_THRESH_HIGH){             //change in accel x < -10
-       Serial.println("jolt x left");
-        //Serial1.println("jolt x left");
-        Serial1.println("LEFT");
-   } else if (change_x > JERK_THRESH_HIGH){              //change in accel x > 10
-       Serial.println("jolt x right");
-      // Serial1.println("jolt x left");
-       Serial1.println("RIGHT");
-   }
-//  if (false) {
-//
-//  }
+ if (false) {
+
+ }
   else {
       if (event.gyro.y > 8 && event.gyro.y < 9){
       Serial.println("down");
@@ -126,16 +112,22 @@ void loop() {
     // Serial.println("");
     // Serial1.println("");
   }
-  change_y = curr_y-prev_y;
-  change_x = curr_x-prev_x;
-  change_z = curr_z-prev_z;
 
-  change_x_avg += change_x;
-  change_y_avg += change_y;
-  change_z_avg += change_y;
+  if (jolt_cooldown-- <= 0) {
+    // Only start averaging if we are off the cooldown
+    change_y = curr_y-prev_y;
+    change_x = curr_x-prev_x;
+    change_z = curr_z-prev_z;
 
-  if (jolt_counter++ % JOLT_WINDOW_MULTIPLIER == 0) {
-    check_jerk(&change_x_avg, &change_y_avg, &change_z_avg);
+    change_x_avg += change_x;
+    change_y_avg += change_y;
+    change_z_avg += change_z;
+    
+    if (jolt_counter++ % JOLT_WINDOW_MULTIPLIER == 0) {
+      if (check_jerk(&change_x_avg, &change_y_avg, &change_z_avg)) {
+        jolt_cooldown = JOLT_COOLDOWN_MAX;
+      }
+    }
   }
 
   prev_y = event.acceleration.y;
@@ -146,32 +138,62 @@ void loop() {
   delay(TIMEFRAME);
 }
 
-void check_jerk(double *x_avg_jerk, double *y_avg_jerk, double *z_avg_jerk) {
+bool check_jerk(double *x_avg_jerk, double *y_avg_jerk, double *z_avg_jerk) {
+  bool fired = false;
   double x_avg = *x_avg_jerk / JOLT_WINDOW_MULTIPLIER;
   double y_avg = *y_avg_jerk / JOLT_WINDOW_MULTIPLIER;
   double z_avg = *z_avg_jerk / JOLT_WINDOW_MULTIPLIER;
-  // Serial.println(x_avg);
-  // Serial.println(y_avg);
-  // Serial.println(z_avg);
-  if (abs(x_avg) < JERK_THRESH_HIGH && abs(y_avg) < JERK_THRESH_HIGH) {
+  double norm = sqrt(x_avg * x_avg + y_avg * y_avg + z_avg * z_avg);
+  if (norm < JERK_THRESH_HIGH && norm < JERK_THRESH_HIGH) {
     //Nothing
-  } else if (abs(x_avg) > abs(y_avg) && abs(x_avg) > abs(z_avg)) {
-    if (x_avg >= 0.0) {
-      Serial.println("jolt right");
-      Serial1.println("RIGHT");
-    } else {
-      Serial.println("jolt left");
-      Serial1.println("LEFT");
-    }
-  } else if (abs(y_avg) > abs(x_avg) && abs(y_avg) > abs(z_avg)) {
-    if (y_avg >= 0.0) {
-      Serial.println("jolt up");
-      Serial1.println("UP");
-    } else {
-      Serial.println("jolt down");
-      Serial1.println("DOWN");
-    }
+    // Serial.print("****x: ");
+    // Serial.println(x_avg);
+    // Serial.print("****y: ");
+    // Serial.println(y_avg);
+  // } else {
+  //   // Find the orientation and determint the event thusly
+  //   Serial.print(event.orientation.x);
+  //   Serial.print(", ");
+  //   Serial.print(event.orientation.y);
+  //   Serial.print(", ");
+  //   Serial.println(event.orientation.z);
+} else if (event.gyro.x < -3) {
+    Serial.println("left-jerk");
+    Serial1.println("LEFT");
+  } else if (event.gyro.x > 3) {
+    Serial.println("right-jerk");
+    Serial1.println("RIGHT");
+  } else if (event.gyro.y < -3) {
+    Serial.println("down-jerk");
+    Serial1.println("DOWN");
+  } else if (event.gyro.y > 3) {
+    Serial.println("up-jerk");
+    Serial1.println("UP");
   }
+  // } else if (abs(x_avg) > abs(y_avg) && abs(x_avg) > abs(z_avg)) {
+  //   if (x_avg >= 0.0) {
+  //     Serial.println("jolt right");
+  //     Serial1.println("RIGHT");
+  //   } else {
+  //     Serial.println("jolt left");
+  //     Serial1.println("LEFT");
+  //   }
+  //   Serial.println(x_avg);
+  //   Serial.print("--------------");
+  //   Serial.println(y_avg);
+  //   fired = true;
+  // } else if (abs(y_avg) > abs(x_avg) && abs(y_avg) > abs(z_avg)) {
+  //   if (y_avg >= 0.0) {
+  //     Serial.println("jolt up");
+  //     Serial1.println("UP");
+  //   } else {
+  //     Serial.println("jolt down");
+  //     Serial1.println("DOWN");
+  //   }
+  //   Serial.println(y_avg);
+  //   Serial.println("--------------");
+  //   fired = true;
+  // }
 
   *x_avg_jerk = 0.0;
   *y_avg_jerk = 0.0;
